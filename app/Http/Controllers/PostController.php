55a2f0168baac7c78d656ed155d89c3d74f872e9
yasser-aboufaris <azertyasser321@gmail.com>
2025-03-21 10:55:41 +0000

    set up the comments handling with the reposotory pattern

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,17 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return response()->json($posts);
+    }
+
+    public function show($id)
+    {
+        $post = Post::find($id);
+        if (!$post) {
+            return response()->json(['message' => 'does not exist'], 404);
+        }
+        return response()->json($post);
     }
 
     /**
@@ -21,30 +31,60 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        
-    }
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'owner_id' => 'required|exists:users,id',
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Post $post)
-    {
-        
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'owner_id' => $request->owner_id,
+        ]);
+
+        return response()->json($post, 201);
     }
+    
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, $id)
     {
-        //
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response()->json(['message' => 'Post not found'], 404);
+        }
+
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'owner_id' => 'required|exists:users,id',
+        ]);
+
+        $post->update([
+            'title' => $request->title,
+            'content' => $request->content,
+            'owner_id' => $request->owner_id,
+        ]);
+
+        return response()->json($post);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        if (!$post) {
+            return response()->json(['message' => 'Post not found'], 404);
+        }
+        $post->delete();
+
+        return response()->json(['message' => 'Post deleted successfully']);
     }
 }

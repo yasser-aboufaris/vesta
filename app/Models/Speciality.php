7872dfe4fc7ae37de040ea5653c5faf674641a39
yasser-aboufaris <azertyasser321@gmail.com>
2025-03-28 10:05:03 +0000

    remove the services folder

--- app/Models/Speciality.php
+++ app/Models/Speciality.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Speciality extends Model
 {
-    //
+    protected $fillable = ['name', 'description'];
+
+    public function Test()
+    {
+        return $this->belongsToOne(Test::class);
+    }
+
+    public function Question()
+    {
+        return $this->hasMany(Question::class);
+    }
+
 }

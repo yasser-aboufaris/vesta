627d9daf5856e91cba089c8188f180f1fef161a5
yasser-aboufaris <azertyasser321@gmail.com>
2025-04-05 14:58:49 +0000

    light changes

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,16 +8,13 @@ use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    // Declare the property outside the constructor
     private CommentRepository $commentRepository;
 
-    // Constructor to inject the CommentRepository
     public function __construct(CommentRepository $commentRepository)
     {
         $this->commentRepository = $commentRepository;
     }
 
-    // Insert a new comment
     public function store(Request $request, $postId)
     {
         $validated = $request->validate([
@@ -34,21 +31,16 @@ class CommentController extends Controller
         return response()->json($comment, 201);
     }
 
-    // Delete a comment
     public function destroy($commentId)
     {
-        // Find the comment by its ID
         $comment = Comment::find($commentId);
 
-        // If the comment doesn't exist, return an error
         if (!$comment) {
             return response()->json(['message' => 'Comment not found'], 404);
         }
 
-        // Delete the comment
         $comment->delete();
 
-        // Return a success response
         return response()->json(['message' => 'Comment deleted successfully'], 200);
     }
 }

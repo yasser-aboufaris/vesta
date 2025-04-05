073d95a1845c1a863cdb5a6d20f393e7472abe62
yasser-aboufaris <azertyasser321@gmail.com>
2025-04-05 13:20:30 +0000

    _

--- app/Repositories/Interfaces/CommentRepositoryInterface.php
+++ app/Repositories/Interfaces/CommentRepositoryInterface.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Interfaces;
 interface CommentReposotoryInterface
 {
     public function GetByPost();
+    public function GetByUser();
     public function create(array $data,$idPost);
     public function delete($id);
 }
